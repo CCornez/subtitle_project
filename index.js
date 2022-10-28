8c9b625801345ca8b2984e384c5dc2249cb4944f
Cyril Cornez <cyrilcornez@outlook.com>
2022-10-28 15:15:25 +0200

    pkg module

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import fs from 'fs';
+import rl from 'readline-sync';
 
-const folderToScrape = process.argv[2];
+const folderToScrape =
+  process.argv[2] ||
+  rl.question('provide a path to you your subtitle folder...\n');
 
 if (!fs.existsSync(folderToScrape)) {
   throw 'folder could not be found !!!';
@@ -10,7 +13,7 @@ const folderContent = fs.readdirSync(folderToScrape);
 
 let allContent = '';
 for (let i = 0; i < folderContent.length; i++) {
-  allContent += fs.readFileSync(folderToScrape + folderContent[i]);
+  allContent += fs.readFileSync(folderToScrape + '/' + folderContent[i]);
 }
 
 allContent = allContent
